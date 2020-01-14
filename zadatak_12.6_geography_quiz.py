d3c1c0cd3ecc2dc9e5c011dcec4dbfffa633f35f
Maja Vukadin <maja.barisic5@gmail.com>
2020-01-14 16:11:59 +0100

    initial commit

---
+++ zadatak_12.6_geography_quiz.py
@@ -0,0 +1,17 @@
+import random
+
+countries_cities = {"Afghanistan": "Kabul", "Albania": "Tirana", "Algeria": "Algiers", "Andorra": "Andorra la Vella", "Angola": "Luanda", "Antigua and Barbuda": "Saint John's", "Argentina": "Buenos Aires",
+                    "Armenia": "Yerevan", "Australia": "Canberra", "Austria": "Vienna", "Azerbaijan": "Baku", "Bahamas": "Nassau", "Bahrain": "Manama", "Bangladesh": "Dhaka", "Barbados": "Bridgetown",
+                    "Belarus": "Minsk", "Belgium": "Brussels", "Belize": "Belmopan", "Benin": "Porto-Novo", "Bhutan": "Thimphu", "Bolivia": "Sucre", "Bosnia and Herzegovina": "Sarajevo", "Botswana": "Gaborone",
+                    "Brazil": "Brasilia", "Brunei": "Bandar Seri Begawan", "Bulgaria": "Sofia", "Burkina Faso": "Ouagadougou", "Burundi": "Gitega", "Cabo Verde": "Praia", "Cambodia": "Phnom Penh",
+                    "Cameroon": "Yaounde", "Canada": "Ottawa","Central African Republic": "Bangui", "Chad": "N'Djamena", "Chile": "Santiago", "China": "Beijing", "Colombia": "Bogotá", "Comoros": "Moroni",
+                    "Congo, Democratic Republic of the": "Kinshasa", "Congo, Republic of the": "Brazzaville","Costa Rica": "San Jose", "Cote d'Ivoire": "Yamoussoukro", "Croatia": "Zagreb", "Cuba": "Havana",
+                    "Cyprus": "Nicosia", "Czechia": "Prague","Denmark": "Copenhagen", "Djibouti": "Djibouti", "Dominica": "Roseau", "Dominican Republic": "Santo Domingo"}
+
+for country in countries_cities.items():
+    country = random.choice(list(countries_cities.keys()))
+    game = input("Game: What is the capital city of {0}?".format(country))
+    if game == countries_cities.get(country):
+        print("Game: This is correct!")
+    else:
+        print("Game: This is wrong!")
