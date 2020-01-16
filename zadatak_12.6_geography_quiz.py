836d59784018fa3744159d704f921122d53a30a8
Maja Vukadin <maja.barisic5@gmail.com>
2020-01-16 15:15:29 +0100

    adding while loop with  A or B option

--- zadatak_12.6_geography_quiz.py
+++ zadatak_12.6_geography_quiz.py
@@ -6,12 +6,22 @@ countries_cities = {"Afghanistan": "Kabul", "Albania": "Tirana", "Algeria": "Alg
                     "Brazil": "Brasilia", "Brunei": "Bandar Seri Begawan", "Bulgaria": "Sofia", "Burkina Faso": "Ouagadougou", "Burundi": "Gitega", "Cabo Verde": "Praia", "Cambodia": "Phnom Penh",
                     "Cameroon": "Yaounde", "Canada": "Ottawa","Central African Republic": "Bangui", "Chad": "N'Djamena", "Chile": "Santiago", "China": "Beijing", "Colombia": "Bogotá", "Comoros": "Moroni",
                     "Congo, Democratic Republic of the": "Kinshasa", "Congo, Republic of the": "Brazzaville","Costa Rica": "San Jose", "Cote d'Ivoire": "Yamoussoukro", "Croatia": "Zagreb", "Cuba": "Havana",
-                    "Cyprus": "Nicosia", "Czechia": "Prague","Denmark": "Copenhagen", "Djibouti": "Djibouti", "Dominica": "Roseau", "Dominican Republic": "Santo Domingo"}
+                    "Cyprus": "Nicosia", "Czechia": "Prague", "Denmark": "Copenhagen", "Djibouti": "Djibouti", "Dominica": "Roseau", "Dominican Republic": "Santo Domingo"}
+def play_game():
+    for country in countries_cities.items():
+        country = random.choice(list(countries_cities.keys()))
+        game = input("Game: What is the capital city of {0}? ".format(country))
+        if game == countries_cities.get(country):
+            print("Game: This is correct!")
+            break
+        else:
+            print("Game: This is wrong!")
 
-for country in countries_cities.items():
-    country = random.choice(list(countries_cities.keys()))
-    game = input("Game: What is the capital city of {0}?".format(country))
-    if game == countries_cities.get(country):
-        print("Game: This is correct!")
+while True:
+    selection = input("Would you like to A) play a new game or B) quit? ")
+
+    if selection.upper() == "A":
+        play_game()
     else:
-        print("Game: This is wrong!")
+        print("Game: Goodbye!")
+        break
